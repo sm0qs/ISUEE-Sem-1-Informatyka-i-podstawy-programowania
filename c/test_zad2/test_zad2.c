46c9e98cb19c2726bd1a825a57dc6131849d4f58
sm0qs <74928880+sm0qs@users.noreply.github.com>
2025-11-19 22:51:21 +0100

    feat(test_zad2): implement cosine calculation using Maclaurin series

---
+++ c/test_zad2/test_zad2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+#if !defined(M_PI)
+#define M_PI 3.14159265358979323846
+#endif // M_PI
+
+int main()
+{
+    double kat, dokladnosc;
+
+    printf("Program wyznacza cos(x) dla x podanego w stopniach\n");
+    printf("z zadana dokladnoscia z uzyciem rozwiniecia w szereg Maclaurina.\n\n");
+
+    do
+    {
+        printf("Podaj x w stopniach: ");
+        fflush(stdin);
+    } while (!scanf("%lf", &kat));
+    kat = (kat / 180) * M_PI;
+
+    do
+    {
+        printf("Podaj dokladnosc (eps): ");
+        fflush(stdin);
+    } while (!scanf("%lf", &dokladnosc));
+
+    double wyraz = 1 /*wtraz ciągu*/, suma = 1 /*suma ciągu*/;
+    int n = 0 /*indeks wyrazu*/, liczbaWyrazow = 1;
+
+    while (fabs(wyraz) > dokladnosc)
+    {
+        double licznik = -kat * kat;
+        double mianownik = (2 * n + 2) * (2 * n + 1);
+
+        wyraz = wyraz * (licznik / mianownik);
+
+        suma = suma + wyraz;
+
+        n = n + 1;
+        liczbaWyrazow = liczbaWyrazow + 1;
+    }
+
+    printf("cos(x) =  %lf\n", suma);
+    printf("Uwzgledniona liczba wyrazow szeregu =  %d\n", liczbaWyrazow);
+    return 0;
+}
